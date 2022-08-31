587f823282d0e3cacb3298b4d4bf26ec0f57f9e1
Far-sa <fmohtasham82@gmail.com>
2022-08-31 18:08:57 +0300

    Modify Err Handler

--- app/server.js
+++ app/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const morgan = require('morgan')
+const createError = require('http-errors')
 const path = require('path')
 
 module.exports = class Application {
@@ -50,17 +51,17 @@ module.exports = class Application {
 
   errorHandler () {
     this.#app.use((req, res, next) => {
-      return res.status(404).json({
-        statusCode: 404,
-        message: 'Page you are looking for does not exist'
-      })
+      next(createError.NotFound('Page you are looking for not found'))
     })
     this.#app.use((error, req, res, next) => {
-      const statusCode = error.status || 500
-      const message = error.message || 'Internal Server error occurred'
+      const serverError = createError.InternalServerError()
+      const statusCode = error.status || serverError.status
+      const message = error.message || serverError.message
       return res.status(statusCode).json({
-        statusCode,
-        message
+        errors: {
+          statusCode,
+          message
+        }
       })
     })
   }
