3aa4d4cc5cb8b732664754a4f33591398739dc7f
Far-sa <fmohtasham82@gmail.com>
2022-10-20 09:24:16 +0300

    swagger schemas ...

--- app/routes/admin/swagger/courseSwagger.js
+++ app/routes/admin/swagger/courseSwagger.js
@@ -14,6 +14,56 @@
  *                   -   true
  *                   -   false
  */
+
+/**
+ * @swagger
+ *  definitions:
+ *      ListOfCourses:
+ *          type: object
+ *          properties:
+ *              statusCode:
+ *                  type: integer
+ *                  example: 200
+ *              data:
+ *                  type: object
+ *                  properties:
+ *                      courses:
+ *                          type: array
+ *                          items:
+ *                              type: object
+ *                              properties:
+ *                                  _id:
+ *                                      type: string
+ *                                      example: "62822e4ff68cdded54aa928d"
+ *                                  title:
+ *                                      type: string
+ *                                      example: "title of course"
+ *                                  short_text:
+ *                                      type: string
+ *                                      example: "summary text of course"
+ *                                  text:
+ *                                      type: string
+ *                                      example: "text and describe of course"
+ *                                  status:
+ *                                      type: string
+ *                                      example: "notStarted | Completed | Holding"
+ *                                  time:
+ *                                      type: string
+ *                                      example: "01:22:34"
+ *                                  price:
+ *                                      type: integer
+ *                                      example: 250,000
+ *                                  discount:
+ *                                      type: integer
+ *                                      example: 20
+ *                                  studendtCount:
+ *                                      type: integer
+ *                                      example: 340
+ *                                  teacher:
+ *                                      type: string
+ *                                      example: "Mack Bill"
+ */
+
 /**
  * @swagger
  *  components:
@@ -80,6 +130,10 @@
  *          responses :
  *              200:
  *                  description: success
+ *                  content:
+ *                      application/json:
+ *                          schema:
+ *                              $ref: '#/definitions/ListOfCourses'
  */
 
 //? Create Course
